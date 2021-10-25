7e0b9dfb4917759b005aa85fcf152c59c8aaf11c
aman <aman>
2021-10-25 23:36:36 +0530

    landing page

---
+++ lib/widgets/custom_button.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class CustomButton extends StatelessWidget {
+  final String title;
+  final VoidCallback onPressed;
+
+  const CustomButton({
+    required this.title,
+    required this.onPressed,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      elevation: 6.0,
+      child: MaterialButton(
+        color: Colors.purple,
+        onPressed: onPressed,
+        child: Text(title),
+      ),
+    );
+  }
+}
---
+++ lib/widgets/widget_home.dart
@@ -0,0 +1,65 @@
+import 'package:felixir/widgets/custom_button.dart';
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  static const id = 'HOME';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        title: const Text('FElixir',
+            style: TextStyle(
+              color: Colors.purple,
+              fontWeight: FontWeight.bold,
+              fontSize: 32.0,
+            )),
+        backgroundColor: Colors.black,
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: const <Widget>[
+              Padding(
+                padding: EdgeInsets.all(16.0),
+                child: Hero(
+                    tag: 'logo',
+                    child: CircleAvatar(
+                      backgroundImage: AssetImage('assets/images/logo.jpeg'),
+                      radius: 70.0,
+                    )),
+              ),
+              Text(
+                'FElixir',
+                style: TextStyle(
+                  color: Colors.purple,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 32.0,
+                ),
+              )
+            ],
+          ),
+          const SizedBox(height: 16.0),
+          CustomButton(
+            title: 'Login',
+            onPressed: () {
+              print('login pressed');
+            },
+          ),
+          const SizedBox(height: 16.0),
+          CustomButton(
+            title: 'Register',
+            onPressed: () {
+              print('register pressed');
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
